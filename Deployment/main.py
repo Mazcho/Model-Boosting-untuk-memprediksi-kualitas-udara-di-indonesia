dff80d2313a65c2d6ec8850e7ef78f31cabd1d3b
Mazcho <putrantonicholaus@gmail.com>
2023-12-09 16:30:34 +0700

    update deployment

---
+++ Deployment/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from PIL import image
+from sklearn.preprocessing import MinMaxScaler
+from catboost import CatBoostClassifier
