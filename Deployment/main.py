1c0d38f23cc440a2521d83a83cc924bc96463743
Mazcho <putrantonicholaus@gmail.com>
2023-12-12 16:21:56 +0700

    progress modeling dan deployment

--- Deployment/main.py
+++ Deployment/main.py
@@ -53,29 +53,44 @@ if menuweb == "App":
     col8, col9 = st.columns(2)
 
     with col8:
-        pM25 = st.number_input("Masukkan PM25: ", value=0)
-        pM10 = st.number_input("Masukkan PM10: ", value=0)
-        sO2 = st.number_input("Masukkan SO2: ", value=0)
-        cO = st.number_input("Masukkan CO: ", value=0)
-        o3 = st.number_input("Masukkan O3: ", value=0)
-        nO2 = st.number_input("Masukkan NO2: ", value=0)
+        pM25 = st.number_input("Masukkan PM25: ", value=0.0, step=0.1)
+        pM10 = st.number_input("Masukkan PM10: ", value=0.0, step=0.1)
+        sO2 = st.number_input("Masukkan SO2: ", value=0.0, step=0.1)
+        cO = st.number_input("Masukkan CO: ", value=0.0, step=0.1)
+        o3 = st.number_input("Masukkan O3: ", value=0.0, step=0.1)
+        nO2 = st.number_input("Masukkan NO2: ", value=0.0, step=0.1)
 
-        # Membuat DataFrame dari input pengguna
-        df_user = pd.DataFrame({
-            'PM2.5': [pM25],
-            'PM10': [pM10],
-            'SO2': [sO2],
-            'CO': [cO],
-            'O3': [o3],
-            'NO2': [nO2]
-        })
+        prediksi_udara=""
+        if st.button("Prediksi Udara : "):
+            prediksi_udara = model.predict([[pM25,pM10,sO2,cO,o3,nO2]])
+    
+    with col9:
+        if prediksi_udara==0:
+            st.write(prediksi_udara)
+            st.write("Baik")
+        elif prediksi_udara==1:
+            st.write(prediksi_udara)
+            st.write("Sedang")
+        elif prediksi_udara==2:
+            st.write(prediksi_udara)
+            st.write("Tidak sehat")
 
-        # # Normalisasi data user (gunakan scaler yang telah di-fit pada data pelatihan)
-        # df_user_normalized = normalisasi_data(df_user)
+        # # Membuat DataFrame dari input pengguna
+        # df_user = pd.DataFrame({
+        #     'PM2.5': [pM25],
+        #     'PM10': [pM10],
+        #     'SO2': [sO2],
+        #     'CO': [cO],
+        #     'O3': [o3],
+        #     'NO2': [nO2]
+        # })
 
-        prediksi_udara = ""
-        if st.button("Prediksi Udara"):
-            prediksi_udara = prediksi_udara(df_user)
+        # # # Normalisasi data user (gunakan scaler yang telah di-fit pada data pelatihan)
+        # # df_user_normalized = normalisasi_data(df_user)
 
-# Tampilkan hasil prediksi di dalam aplikasi
-st.write(prediksi_udara)
+        # prediksi_udara = ""
+        # if st.button("Prediksi Udara"):
+        #     prediksi_udara = prediksi_udara(df_user)
+
+        # # Tampilkan hasil prediksi di dalam aplikasi
+        # st.write(prediksi_udara)
